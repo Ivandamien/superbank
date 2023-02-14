80960d94c636c8af18548b5e3660cd39cc599d3c
Ivan Okello <74784829+Ivandamien@users.noreply.github.com>
2023-02-14 16:16:52 +0300

    Created the bank projects and file structure

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from './style';
+
+const App = () => {
+  return (
+    <div className='bg-primary w-full overflow-hidden'>
+      <h1>Hello, World</h1>
+      
+    </div>
+  )
+}
+
+export default App
